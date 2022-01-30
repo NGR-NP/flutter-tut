c8e8c80d339f58568e93f187b9a9cbaff58e8534
NGR <ngr112001@gmail.com>
2022-01-30 23:26:11 +0545

    day3 commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+
+      // open home page by on app start at first
+      // initialRoute: "/home",
+      
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 10;
+  final String name = "tej";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("NGR App")),
+      body: Center(
+        child: Center(
+          child: Text("welcome to $days days of flutte $name"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
